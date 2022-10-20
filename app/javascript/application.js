2d8842cfa821935627852aba3002fde7b0589d44
Gustavo Carvalho <gsc13@tuta.io>
2022-10-20 15:50:13 -0300

    Add support to React replacing importmap with Webpack

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<h1>Hello World!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
