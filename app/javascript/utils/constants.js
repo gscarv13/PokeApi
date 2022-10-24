41d466eb15b48de3bc9b85cccf80677d631b4a24
Gustavo Carvalho <gsc13@tuta.io>
2022-10-24 01:08:08 -0300

    Add react utils

---
+++ app/javascript/utils/constants.js
@@ -0,0 +1,8 @@
+export const BASE_URL = process.env.BASE_URL;
+export const DASHBOARD_INDEX = "/dashboard";
+export const TRADE_BUY = "/trades/buy";
+export const TRADE_SELL = "/trades/sell";
+
+export const BTC_BASE_URL = "https://api.coinstats.app/public/v1";
+export const BTC_HISTORY = "/charts?period=1m&coinId=bitcoin";
+export const BTC_INFO = "/coins/bitcoin?currency=USD";
