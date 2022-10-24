d092e9fa50f63458ac8923d737dd264ba72e4418
Gustavo Carvalho <gsc13@tuta.io>
2022-10-24 00:35:24 -0300

    Add React Configuration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx'
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render( <App/>);
